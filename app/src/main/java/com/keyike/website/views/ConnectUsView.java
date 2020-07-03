f19d051819d075b7bb5da624008c0913f744619a
lufeisong <lufeisong123>
2020-07-03 18:54:36 +0800

    1.添加主页2

--- app/src/main/java/com/keyike/website/views/ConnectUsView.java
+++ app/src/main/java/com/keyike/website/views/ConnectUsView.java
@@ -22,7 +22,7 @@ import java.util.List;
 
 public class ConnectUsView extends LinearLayout {
     String[] telTypes = {"咨询电话:","免费试镜:","报名咨询:","经纪合作:","机构合作:","品牌合作:","商务合作:"};
-    String[] telNums = {"021-6587","免费试镜","报名咨询","经纪合作","机构合作","品牌合作","商务合作"};
+    String[] telNums = {"021-65876552","免费试镜","报名咨询","经纪合作","机构合作","品牌合作","商务合作"};
     TextView tv_tel_1,tv_tel_2,tv_tel_3,tv_tel_4,tv_tel_5,tv_tel_6,tv_tel_7;
     public ConnectUsView(Context context) {
         super(context);
@@ -51,8 +51,39 @@ public class ConnectUsView extends LinearLayout {
 
         SpannableString sp1 = new SpannableString(telTypes[0] + telNums[0]);
         sp1.setSpan(new MySpanClick(1),6,sp1.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tv_tel_1.setText(sp1);
         tv_tel_1.setMovementMethod(LinkMovementMethod.getInstance());
-        tv_tel_1.setText(sp1.toString());
+
+        SpannableString sp2 = new SpannableString(telTypes[1] + telNums[1]);
+        sp2.setSpan(new MySpanClick(2),6,sp2.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tv_tel_2.setText(sp2);
+        tv_tel_2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        SpannableString sp3 = new SpannableString(telTypes[2] + telNums[2]);
+        sp3.setSpan(new MySpanClick(3),6,sp3.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tv_tel_3.setText(sp3);
+        tv_tel_3.setMovementMethod(LinkMovementMethod.getInstance());
+
+        SpannableString sp4 = new SpannableString(telTypes[3] + telNums[3]);
+        sp4.setSpan(new MySpanClick(4),6,sp4.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tv_tel_4.setText(sp4);
+        tv_tel_4.setMovementMethod(LinkMovementMethod.getInstance());
+
+        SpannableString sp5 = new SpannableString(telTypes[4] + telNums[4]);
+        sp5.setSpan(new MySpanClick(5),6,sp5.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tv_tel_5.setText(sp5);
+        tv_tel_5.setMovementMethod(LinkMovementMethod.getInstance());
+
+        SpannableString sp6 = new SpannableString(telTypes[5] + telNums[5]);
+        sp6.setSpan(new MySpanClick(6),6,sp6.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tv_tel_6.setText(sp6);
+        tv_tel_6.setMovementMethod(LinkMovementMethod.getInstance());
+
+        SpannableString sp7 = new SpannableString(telTypes[6] + telNums[6]);
+        sp7.setSpan(new MySpanClick(7),6,sp7.length(), Spanned.SPAN_INCLUSIVE_EXCLUSIVE);
+        tv_tel_7.setText(sp7);
+        tv_tel_7.setMovementMethod(LinkMovementMethod.getInstance());
+
     }
 
     class MySpanClick extends ClickableSpan{
